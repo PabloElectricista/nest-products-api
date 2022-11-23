dd586d7216286633939a40f975f029a26a0c99be
Pablo Spector <pablo_spector@hotmail.com>
2022-11-23 06:16:17 -0300

    database on atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductModule } from './product/product.module';
+import { config } from 'dotenv';
+config();
 
 @Module({
-  imports: [MongooseModule.forRoot(`mongodb://localhost/nestjs-product-api`), ProductModule],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URL), ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
