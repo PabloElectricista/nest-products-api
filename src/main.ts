b8ab06a28212c58dbaac398e67b91a213f6ca88e
Pablo Spector <pablo_spector@hotmail.com>
2022-11-23 04:12:54 -0300

    second commit

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:4200',
-    methods: ['POST', 'PATCH', 'DELETE', 'GET'],
-  });
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
